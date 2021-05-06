db53a059db032677594ea1d30f68998a19615dc3
Charith2021 <83691172+Charith2021@users.noreply.github.com>
2021-05-06 11:09:10 +0530

    Stack commit

---
+++ Stack/Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Stack nums=new Stack();
+        nums.push(10);
+        nums.push(20);
+        nums.push(30);
+        nums.push(40);
+
+        nums.show();
+    }
+}
---
+++ Stack/Stack.java
@@ -0,0 +1,15 @@
+public class Stack {
+
+        int stack[]=new int[5];
+        int top=0;
+        
+        public void push(int data)
+        {
+            stack[top]=data;
+            top++;
+        }
+
+
+    public void show() {
+    }
+}
